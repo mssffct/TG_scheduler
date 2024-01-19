cb68e84a828479d61a181395912fd7aa110e6e42
mssffct <herooftheday82@mail.ru>
2024-01-19 17:47:42 +0300

    docs(scheduler_backend/memos) docs for create and list endpoints

--- scheduler_backend/memos/views.py
+++ scheduler_backend/memos/views.py
@@ -4,23 +4,34 @@ from rest_framework.request import Request
 from django.db import transaction
 from rest_framework.decorators import action
 from django.db.models import QuerySet
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .serializers import MemosSerializer
 from .models import Memo
+from .docs_schema import memo_schema
 from backend.responses import *
+from backend.docs_schema import *
 from backend.mixins import MixedPermissionModelViewSet
 from backend.permissions import IsCreator
 
 
 class MemosViewSet(MixedPermissionModelViewSet):
+
     serializer_class = MemosSerializer
     permission_classes_by_action = {
-        ('list', 'create', 'partial_update', 'destroy'): [IsCreator, ]
+        ('create', 'partial_update', 'destroy'): [IsCreator, ],
     }
 
     def get_queryset(self) -> QuerySet:
+        if getattr(self, "swagger_fake_view", False):
+            return Memo.objects.none()
         return Memo.objects.filter(creator=self.request.app_user).all()
 
+    @swagger_auto_schema(request_body=memo_schema, responses={
+        200: openapi.Response("Success memo create", schema=success_response),
+        503: openapi.Response("Failed to create memo", schema=success_response)
+    })
     def create(self, request: Request, *args, **kwargs) -> SuccessResponse | ErrorResponse:
         data = request.data
         try:
@@ -29,10 +40,15 @@ class MemosViewSet(MixedPermissionModelViewSet):
                     creator=request.app_user, **data
                 )
                 memo.save()
-                return SuccessResponse('Saved successfully')
+                return SuccessResponse(data={'message': 'Saved successfully'})
         except Exception as ex:
             print(ex)
             return ErrorResponse(description='Failed to save memo')
+
+    @swagger_auto_schema(auto_schema=None)
+    def update(self, request, *args, **kwargs):
+        return ErrorResponse(status=405)
+
 
     def retrieve(self, request, *args, **kwargs):
         #TODO Memo get function
