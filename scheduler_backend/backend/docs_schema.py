31dc52121c264e8084410b5ba61e48363572a0e8
mssffct <herooftheday82@mail.ru>
2024-03-03 22:48:56 +0300

    docs(scheduler/backend) doc schema constructor

--- scheduler_backend/backend/docs_schema.py
+++ scheduler_backend/backend/docs_schema.py
@@ -7,3 +7,17 @@ def get_error_response(description: str) -> Schema:
 
 def get_success_response(description: str) -> Schema:
 	return Schema(type=TYPE_OBJECT, properties={'message': Schema(type=TYPE_STRING, description=description)})
+
+
+class DocsSchemaConstructor:
+	def __init__(self, schema: bool = None, *args):
+		self.construct_docs_schema(schema, args)
+
+	@staticmethod
+	def construct_docs_schema(schema: bool, args) -> dict | Schema:
+		docs_schema = {}
+		for item in args:
+			docs_schema[item[0]] = Schema(type=item[1], description=item[2])
+		if schema:
+			return Schema(type=TYPE_OBJECT, properties=docs_schema)
+		return docs_schema
